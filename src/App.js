8758dd940aec27533a55a5620043122696f534f7
Satyamgithub11 <satyamkumar823@gmail.com>
2024-08-14 10:24:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine'  ;
+
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+             height="100vh"
+             projectID = "292e70f5-8285-4523-b70d-37430a041fa5"
+             userName = "satyamkumar823@gmail.com"
+             userSecret = "Chintu@11"
+        />
+    )
+}
+
+
+export default App
+
